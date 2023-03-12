a939bb084af9a896bb339761b885fc56a5ea1038
Exisi <exi.simat@gmail.com>
2023-03-12 20:49:47 +0800

    🦄 refactor(all): 完全重构，优化大量代码，增加可读习性

--- js/deduplicate.js
+++ js/deduplicate.js
@@ -1,151 +1,129 @@
 (function () {
+	"use strict";
 	let timer;
 	let DOM = {
-		row: document.querySelectorAll(".row-rule"),
-		inline: document.querySelectorAll(".inline-rule"),
-		split: document.getElementById("split"),
-		type: document.getElementById("output-type"),
-		input: document.querySelectorAll("#input,.split"),
-		inline: document.getElementsByClassName("inline-rule"),
 		container: document.getElementById("center"),
-		inText: document.getElementById("input"),
-		outText: document.getElementById("output"),
+		checkebox: {
+			multiline: {
+				unique: document.querySelector(".row-rule[name='unique-multiline']"),
+				notBlank: document.querySelector(".row-rule[name='remove-blank']"),
+			},
+			inline: {
+				unique: document.querySelector(".inline-rule[name='unique-inline']"),
+				expandAll: document.querySelector(".inline-rule[name='expand-to-all']"),
+				pre_suf_blank: document.querySelector(".inline-rule[name='pre-suf-blank']"),
+			},
+		},
+		rule: {
+			split: document.querySelector(".rule[name='split']"),
+		},
+		textarea: {
+			input: document.getElementById("input"),
+			output: document.getElementById("output"),
+		},
+		select: document.getElementsByTagName("select")[0],
 	};
-	DOM.inline[1].disabled = true;
-	DOM.inline[3].disabled = true;
+	DOM.checkebox.inline.expandAll.disabled = true;
 	DOM.container.addEventListener("click", (e) => {
 		let tag = e.target.tagName.toLowerCase();
-		if (tag == "input" || tag == "select") setText();
+		if (tag == "input" || tag == "select") {
+			setText();
+		}
 	});
-	DOM.input.forEach((e) => {
-		e.addEventListener("keydown", () => {
+
+	for (const item in DOM.textarea) {
+		DOM.textarea[item].addEventListener("keydown", () => {
 			clearTimeout(timer);
 			timer = setTimeout(() => {
 				setText();
 			}, 100);
 		});
-	});
+	}
 
 	function setText() {
-		let v = DOM.inText.value == null ? "" : DOM.inText.value;
-		let config = getConfig(DOM);
-		if (!(config.line || config.inline)) {
+		let rule = getRule();
+		let originList = getOriginRowList();
+		let formattedList = textFormatter(originList, rule);
+		DOM.textarea.output.innerHTML = restoreListToString(formattedList, rule);
+	}
+
+	function getOriginRowList() {
+		let text = DOM.textarea.input.value;
+		text = text == null ? "" : text;
+		return text.split("\n");
+	}
+
+	function textFormatter(list, rule) {
+		if (rule.unique.inline) {
+			let newList = list.map((row) => row.split(rule.split));
+			let unique_list = [];
+			newList = newList.map((row) => {
+				return row.filter((word, index) => {
+					if (rule.expandAll && unique_list.includes(word)) {
+						return false || word == "";
+					}
+					unique_list.push(word);
+					//unique word, but save the "" word
+					return row.indexOf(word) == index || word == "";
+				});
+			});
+
+			if (rule.notBlank.pre_suf) {
+				newList = newList.map((row) => {
+					return row.map((words) => words.trim());
+				});
+			}
+
+			list = newList.map((row) => row.join(rule.split));
+		}
+
+		if (rule.unique.multiline) {
+			if (rule.notBlank.pre_suf) {
+				list = list.map((row) => row.trim());
+			}
+			list = list.filter((row, index) => {
+				//unique row, but save the "" row
+				return list.indexOf(row) === index || row == "";
+			});
+		}
+
+		list = rule.notBlank.row ? list.filter((row) => row != "") : list;
+		return list;
+	}
+
+	function getRule() {
+		DOM.checkebox.inline.expandAll.disabled = true;
+		let rule = {
+			unique: {
+				inline: DOM.checkebox.inline.unique.checked,
+				multiline: DOM.checkebox.multiline.unique.checked,
+			},
+			notBlank: {
+				row: DOM.checkebox.multiline.notBlank.checked,
+				pre_suf: DOM.checkebox.inline.pre_suf_blank.checked,
+			},
+			split: DOM.rule.split.value != "" ? DOM.rule.split.value : " ",
+			expandAll: DOM.checkebox.inline.expandAll.checked,
+			formatType: DOM.select.value,
+		};
+		if (!(rule.unique.inline || rule.unique.multiline)) {
 			return window.alert("请至少选中一种去重方式");
 		}
-		DOM.outText.innerHTML = modelSetting(v, config);
+		DOM.checkebox.inline.expandAll.disabled = !rule.unique.inline;
+		return rule;
 	}
 
-	/******************************************************************
-	 * format setting
-	 */
-	function getConfig() {
-		DOM.inline[1].disabled = true;
-		let config = {
-			line: DOM.row[0].checked,
-			remove_blank_row: DOM.row[1].checked,
-			inline: DOM.inline[0].checked,
-			toRow: DOM.inline[1].checked,
-			remove_blank: DOM.inline[2].checked,
-			remove_pre_suf_blank: DOM.inline[3].checked,
-			split: DOM.split.value != "" ? DOM.split.value : " ",
-			type: DOM.type.value,
-		};
-		if (config.inline) DOM.inline[1].disabled = false;
-		if (config.type == 2) DOM.inline[3].disabled = false;
-		else DOM.inline[3].disabled = true;
-		return config;
-	}
-
-	function modelSetting(v, c) {
-		/*
-		 * v: text value
-		 * c: format config
-		 */
-		if (c.inline) v = formatToUniqueWord(v, c.split);
-		if (c.line) v = formatToUniqueRow(v, c);
-		if (c.toRow && c.inline) v = formatToAllRow(v, c.split);
-		if (c.remove_blank) v = textRemoveMutiBlank(v, c.split);
-		if (c.type == 1) {
-			v = v.replaceAll("\n", c.split).replaceAll(/(\x20)+/g, " ");
-			if (c.inline) v = formatToUniqueWord(v, c.split);
+	function restoreListToString(list, rule) {
+		let newList = list.map((row) => row.split(rule.split));
+		if (rule.formatType == 1) {
+			return newList.flat(Infinity).join(rule.split);
 		}
-		if (c.type == 2) {
-			v = v.replaceAll("\n", c.split).replaceAll(/(\x20)+/g, " ");
-			v = v.replaceAll(c.split, "\n");
-			if (c.line) v = formatToUniqueRow(v, c);
+
+		if (rule.formatType == 2) {
+			newList = newList.flat(Infinity);
+			newList = rule.notBlank.row ? newList.filter((row) => row != "") : newList;
+			return newList.join("\n");
 		}
-		if (c.remove_blank_row) v = removeBlankRow(v);
-		return v;
-	}
-
-	/******************************************************************
-	 * text dedeuplicate format method
-	 */
-
-	function formatToUniqueRow(v, c) {
-		let rowlist = [];
-		let list = v.split("\n");
-		list.forEach((row) => {
-			row = c.remove_pre_suf_blank ? row.trim() : row;
-			rowlist.push(row);
-		});
-		v = rowlist.join("\n");
-		return [...new Set(v.split("\n"))].join("\n");
-	}
-
-	function formatToUniqueWord(v, s) {
-		let list = v.split("\n");
-		let rowlist = [];
-		list.forEach((row) => {
-			words = row.split(s);
-			rowlist.push([...new Set(words)].join(s));
-		});
-		return rowlist.join("\n");
-	}
-
-	function formatToAllRow(v, s) {
-		let list = v.split("\n");
-		let rowlist = [];
-		let unique_list = [];
-		for (const row of list) {
-			let words = row.split(s);
-			let newline = [];
-			for (const word of words) {
-				if ((!newline.includes(word) || word == "") && !unique_list.includes(word)) {
-					newline.push(word);
-				}
-			}
-			let sets = new Set(words);
-			sets.delete("");
-			let temp = Array.from(sets);
-			unique_list.push.apply(unique_list, temp);
-			unique_list = Array.from(new Set(unique_list));
-			rowlist.push(newline.join(s));
-		}
-		return rowlist.join("\n");
-	}
-
-	/******************************************************************
-	 * text blank format method
-	 */
-	function removeBlankRow(v) {
-		let list = v.split("\n");
-		return list.filter((row) => row != "").join("\n");
-	}
-
-	function textRemoveMutiBlank(v, s) {
-		if (s == " ") {
-			v = v.replaceAll(/(\x20)+/g, " ");
-			let rowlist = [];
-			let list = v.split("\n");
-			for (const row of list) {
-				if (row.indexOf(" ") == 0) row = row.substring(1, temp.length);
-				rowlist.push(row);
-			}
-			v = rowlist.join("\n");
-		} else {
-			v = v.replaceAll(/(\x20)+/g, "").replaceAll("  ", "");
-		}
-		return v;
+		return list.join("\n");
 	}
 })();
